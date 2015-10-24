ddc0b57557b39128776b8e45d19800bc0f8c3244
Daniel Yang <miniflycn@justany.net>
2015-10-24 23:38:01 +0800

    format

--- test/test.ts
+++ test/test.ts
@@ -6,44 +6,44 @@ import fs = require('fs');
 import QFS = require('../src/qfs');
 
 let qfs = QFS({
-	max: 1,
-	files: 'test/feature/**'
+    max: 1,
+    files: 'test/feature/**'
 });
 
 const myContents = 'var a = 123;',
-	newContents = 'var c = 456;';
+    newContents = 'var c = 456;';
 
 describe("QFS", () => {
-	before(function (done) {
-		setTimeout(done, 100);
-	});
-	
-	it('should get a file', () => {
-		let filename = path.join(__dirname, 'feature/my.js'),
-			file = qfs.read(filename);
-		
-		file.contents.toString().should.equal(myContents);
-		qfs.get(filename).should.equal(file);
-	});
-	
-	it('should just cache one file', () => {
-		let fileMe = path.join(__dirname, 'feature/my.js'),
-			fileYou = path.join(__dirname, 'feature/you.js'),
-			file = qfs.read(fileYou);
-			
-		(qfs.get(fileMe) === null).should.be.true;
-	});
-	
-	it('should watch file changed', (done) => {
-		let filename = path.join(__dirname, 'feature/my.js');
-		
-		qfs.read(filename);
-		(qfs.get(filename) === null).should.be.false;
-		
-		fs.writeFileSync(filename, newContents, 'utf-8');
-		fs.writeFileSync(filename, myContents, 'utf-8');
-		
-		(qfs.get(filename) === null).should.be.true;
-		done();
-	});
+    before(function (done) {
+        setTimeout(done, 100);
+    });
+
+    it('should get a file', () => {
+        let filename = path.join(__dirname, 'feature/my.js'),
+            file = qfs.read(filename);
+
+        file.contents.toString().should.equal(myContents);
+        qfs.get(filename).should.equal(file);
+    });
+
+    it('should just cache one file', () => {
+        let fileMe = path.join(__dirname, 'feature/my.js'),
+            fileYou = path.join(__dirname, 'feature/you.js'),
+            file = qfs.read(fileYou);
+
+        (qfs.get(fileMe) === null).should.be.true;
+    });
+
+    it('should watch file changed', (done) => {
+        let filename = path.join(__dirname, 'feature/my.js');
+
+        qfs.read(filename);
+        (qfs.get(filename) === null).should.be.false;
+
+        fs.writeFileSync(filename, newContents, 'utf-8');
+        fs.writeFileSync(filename, myContents, 'utf-8');
+
+        (qfs.get(filename) === null).should.be.true;
+        done();
+    });
 });
--- typings/lru-cache/lru-cache.d.ts
+++ typings/lru-cache/lru-cache.d.ts
@@ -4,31 +4,31 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module 'lru-cache' {
-	function LRU<T>(opts: LRU.Options<T>): LRU.Cache<T>;
-	function LRU<T>(max: number): LRU.Cache<T>;
+    function LRU<T>(opts: LRU.Options<T>): LRU.Cache<T>;
+    function LRU<T>(max: number): LRU.Cache<T>;
 
-	module LRU {
-		interface Options<T> {
-			max?: number;
-			maxAge?: number;
-			length?: (value: T) => number;
-			dispose?: (key: string, value: T) => void;
-			stale?: boolean;
-		}
+    module LRU {
+        interface Options<T> {
+            max?: number;
+            maxAge?: number;
+            length?: (value: T) => number;
+            dispose?: (key: string, value: T) => void;
+            stale?: boolean;
+        }
 
-		interface Cache<T> {
-			set(key: string, value: T): void;
-			get(key: string): T;
-			peek(key: string): T;
-			has(key: string): boolean
-			del(key: string): void;
-			reset(): void;
-			forEach(iter: (value: T, key: string, cache: Cache<T>) => void, thisp?: any): void;
+        interface Cache<T> {
+            set(key: string, value: T): void;
+            get(key: string): T;
+            peek(key: string): T;
+            has(key: string): boolean
+            del(key: string): void;
+            reset(): void;
+            forEach(iter: (value: T, key: string, cache: Cache<T>) => void, thisp?: any): void;
 
-			keys(): string[];
-			values(): T[];
-		}
-	}
+            keys(): string[];
+            values(): T[];
+        }
+    }
 
-	export = LRU;
+    export = LRU;
 }
