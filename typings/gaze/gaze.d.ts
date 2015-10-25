5aad9640deab69acd5da11d5df9a964755b5087e
Daniel Yang <miniflycn@justany.net>
2015-10-25 16:49:21 +0800

    update tad

--- typings/gaze/gaze.d.ts
+++ typings/gaze/gaze.d.ts
@@ -1,13 +1,32 @@
 declare module "gaze" {
-    interface Gaze {
-        watched()
-        on(e: string, cb)
-        add(files: string | string[])
-        remove(filename: string)
-    }
+    
+    function gaze(file: string, cb: (err: Error, watcher: gaze.Gaze) => void)
+    function gaze(files: string[], cb: (err: Error, watcher: gaze.Gaze) => void)
 
-    function gaze(file: string, cb: (err: Error, watcher: Gaze) => void)
-    function gaze(files: string[], cb: (err: Error, watcher: Gaze) => void)
+    module gaze {
+        class Gaze {
+            /**
+             * Get all watched files
+             */
+            watched()
+            /**
+             * on event trigger
+             */
+            on(e: string, cb: (e: string, filepath: string) => void)
+            /**
+             * on event trigger
+             */
+            on(e: string, cb: (filepath: string) => void)
+            /**
+             * Adds file(s) patterns to be watched.
+             */
+            add(files: string | string[])
+            /**
+             * removes a file or directory from being watched.
+             */
+            remove(filename: string)
+        }
+    }
 
     export = gaze;
 }
